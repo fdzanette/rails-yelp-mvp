b1e187533b059d95a91ea9cfda5fa704fc14113d
fabricio zanette <fabzanette@gmail.com>
2018-03-28 15:33:24 -0300

    create yelp app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([
+  {name: "Paladar da Vila",
+    address: "Mourato Coelho",
+    phone_number: "01 30 79 79 79",
+    category: "french" },
+  {name: "Ohka",
+    address: "Professor Carvalho",
+    phone_number: "01 30 79 79 69",
+    category: "japanese" },
+  {name: "PF CHANG",
+    address: "Av JK",
+    phone_number: "01 30 79 79 59",
+    category: "chinese" },
+  {name: "Belga Corner",
+    address: "Bandeira Paulista",
+    phone_number: "01 30 79 79 49",
+    category: "belgian" },
+  {name: "Margherita",
+    address: "Alameda Tietê",
+    phone_number: "01 30 79 79 39",
+    category: "italian" }])
